6401d53f51c5ff077b21803cdb4918f886b95420
Filipe Cruz <filipecesar_1997@icloud.com>
2026-06-23 06:26:24 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<?php 
+            $user = [
+                "user" => [
+                    "id" => 1,
+                    "name" => "Luiz Rebite",
+                    "email" => "luiz.rebite@rpinfo.com",
+                    "profile_thumb" => "https://imagem.com.br",
+                    "isActive" => true
+                ]
+            ];
+
+
+        echo  "meu site<br><br><hr>";
+        print_r($user);
+        echo  "<br><br><hr>";
+        var_dump($user);
+        die();
+        ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minha aula PHP</title>
+</head>
+<body>
+    <h1>
+        
+    </h1>
+</body>
+</html>
