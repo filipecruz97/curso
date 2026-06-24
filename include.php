15071265e2582518b8b0cb260d86cce968b4afc0
Filipe Cruz <filipecesar_1997@icloud.com>
2026-06-23 22:38:50 -0300

    aula 30:12

---
+++ include.php
@@ -0,0 +1,17 @@
+<?php 
+
+declare(strict_types = 1);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <?php include 'head.php'; ?>
+    
+    <title>Include</title>
+</head>
+<body>
+   <h1><?= 'Include' ?></h1>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,33 +1,28 @@
-
-<?php 
-            $user = [
-                "user" => [
-                    "id" => 1,
-                    "name" => "Luiz Rebite",
-                    "email" => "luiz.rebite@rpinfo.com",
-                    "profile_thumb" => "https://imagem.com.br",
-                    "isActive" => true
-                ]
-            ];
-
-
-        echo  "meu site<br><br><hr>";
-        print_r($user);
-        echo  "<br><br><hr>";
-        var_dump($user);
-        die();
-        ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Minha aula PHP</title>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Include e Require</title>
 </head>
 <body>
-    <h1>
-        
-    </h1>
+    <h1>Include e Require</h1>
+
+    <ul>
+        <li>
+
+            <a href="/include.php">Include</a>
+
+        </li>
+
+        <li>
+
+            <a href="/require.php">Require</a>
+
+        </li>
+
+    </ul>
+
 </body>
 </html>
